d3502bec6ec9c9d73d209f75eac69d8181bc985d
TomEnergy <energy.tom1412@gmail.com>
2021-10-14 10:56:20 +0900

    Initial Commit

---
+++ Watermark/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Watermark
+//
+//  Created by 鈴木 智也 on 2021/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
